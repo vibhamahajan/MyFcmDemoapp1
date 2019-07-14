16cd0e4fda89dad30283e8b828efea587d1bd327
vibhamahajan <vibhavari2741990@gmail.com>
2019-07-14 11:17:31 +0530

    write hello world

--- app/src/main/java/com/example/vibhu/myfcmdemoapp/MyFirebaseInstanceIdService.java
+++ app/src/main/java/com/example/vibhu/myfcmdemoapp/MyFirebaseInstanceIdService.java
@@ -18,6 +18,7 @@ public class MyFirebaseInstanceIdService extends FirebaseInstanceIdService {
         // Get updated InstanceID token.
         String refreshedToken = FirebaseInstanceId.getInstance().getToken();
         Log.d(TAG, "Refreshed token: " + refreshedToken);
+        System.out.println("helo world.....");
 
         // TODO: Implement this method to send any registration to your app's servers.
       //  sendRegistrationToServer(refreshedToken);
